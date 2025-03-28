1a7609f4626720cd9702d1dd2836f3520689361e
Ashlesh <ashlesh208@gmail.com>
2025-03-28 12:06:03 +0530

    home page done

---
+++ app/provider.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import React, { useState } from 'react'
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import Header from '@/components/custom/Header'
+import { MessagesContext } from '@/context/MessagesContext';
+
+function Provider({children}) {
+
+    const [messages ,setMessages] = useState();
+
+    return (
+        <div>
+            <MessagesContext.Provider value={{messages, setMessages}}>
+            <NextThemesProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header/>
+            {children}
+            </NextThemesProvider>
+            </MessagesContext.Provider>
+        </div>
+    )
+}
+
+export default Provider
---
+++ components/custom/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+
+import { Button } from '../ui/button';
+import Colors from '@/data/Colors';
+
+function Header() {
+    return(
+        <div className='p-4 flex justify-between items-center'>
+            <Image src={'/vercel.svg'}  alt='Logo' width={40} height={40} />
+            <div className='flex gap-5'>
+                <Button variant="ghost">Sign In</Button>
+                <Button
+                className="text-white" style={{
+                    backgroundColor:Colors.BLUE
+                }}
+                >Get started</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
